649d0b583de7adb0adfc2634ec78dcaeed6dfd5c
Johannes Natter <office@dsp-crowd.com>
2025-04-12 11:33:56 +0200

    tools: Update cppcheck

--- tools/cppcheck.sh
+++ tools/cppcheck.sh
@@ -4,8 +4,8 @@ find \
 	. \
 	-type f \
 	-name "*.cpp" \
-	! -path "./ProcessingCore/stm32-uart/*" \
-	! -path "./ProcessingCore/SystemDebugging.*" \
+	! -path "./deps/ProcessingCore/targets/stm32/SystemDebugging.*" \
+	! -path "./deps/tclap_loc/*" \
 | xargs cppcheck \
 	--language=c++ \
 	--std=c++11 \
