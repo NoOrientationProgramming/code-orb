4f3e1610dd2b99d49e6f0e43bfe302fad92c03c5
Johannes Natter <office@dsp-crowd.com>
2025-04-15 17:04:47 +0200

    main: Beautify

--- src/main.cpp
+++ src/main.cpp
@@ -155,7 +155,7 @@ int main(int argc, char *argv[])
 	aho.package = dPackageName;
 	aho.version = dVersion;
 	aho.nameApp = dAppName;
-	aho.copyright = "(C) 2025 DSP-Crowd Electronics GmbH";
+	aho.copyright = " © 2025 DSP-Crowd Electronics GmbH";
 #endif
 	cmd.setOutput(&aho);
 
